f31a52d194fe25e17e90f46a370e400b820a3224
rsanchezbalo <rsanchezbalo1988@gmail.com>
2016-02-27 10:54:06 +0100

    Blog first post

---
+++ pytho_intro.py
@@ -0,0 +1,10 @@
+def osti(name=None):
+    if not name:
+        print("osti tu! no hay nadie")
+    else:
+        print("osti tu! el " + name)
+
+osti("Paco")
+osti("Manolo")
+osti("Jesus")
+osti()
